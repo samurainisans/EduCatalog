e022c44c29798748fadd0c5e2cd44b51cd4707ca
samurainisans <samurainisa4@gmail.com>
2023-11-14 20:44:09 +0700

    Commit 7: Улучшен дизайн форм

--- educationapp/app/api/profession_routes.py
+++ educationapp/app/api/profession_routes.py
@@ -1,4 +1,6 @@
-from flask import jsonify, request, Blueprint
+from flask import Blueprint, render_template
+from flask import jsonify, request
+from educationapp.app import db
 from educationapp.app.models.profession import Profession
 
 profession_blueprint = Blueprint('profession', __name__)
@@ -7,6 +9,34 @@ profession_blueprint = Blueprint('profession', __name__)
 @profession_blueprint.route('/professions', methods=['GET'])
 def get_professions():
     professions = Profession.query.all()
-    return jsonify([profession.to_dict() for profession in professions])
+    return render_template('professions.html', professions=professions)
 
 
+@profession_blueprint.route('/professions', methods=['POST'])
+def add_profession():
+    name = request.form.get('name')
+
+    if not name:
+        return jsonify({'error': 'No name provided'}), 400
+
+    existing_profession = Profession.query.filter_by(name=name).first()
+    if existing_profession:
+        return jsonify({'error': 'Profession already exists'}), 409
+
+    new_profession = Profession(name=name)
+    db.session.add(new_profession)
+    db.session.commit()
+
+    return render_template('professions.html', professions=Profession.query.all())
+
+
+@profession_blueprint.route('/professions/<int:id>', methods=['DELETE'])
+def delete_profession(id):
+    profession = Profession.query.get(id)
+    if not profession:
+        return jsonify({'error': 'Profession not found'}), 404
+
+    db.session.delete(profession)
+    db.session.commit()
+
+    return jsonify({'message': 'Profession deleted'}), 200
--- educationapp/app/models/profession.py
+++ educationapp/app/models/profession.py
@@ -4,3 +4,9 @@ class Profession(db.Model):
     __tablename__ = 'professions'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
