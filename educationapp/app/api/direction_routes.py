5d5de46ef4437e328efa747bd9da3b94460502b1
samurainisans <samurainisa4@gmail.com>
2023-11-14 17:44:08 +0700

    Commit 5: Добавление направлений и специальностей

--- educationapp/app/api/direction_routes.py
+++ educationapp/app/api/direction_routes.py
@@ -1,24 +1,35 @@
-from flask import jsonify, request, Blueprint, render_template
+from flask import jsonify, request, Blueprint, render_template, url_for
+from werkzeug.utils import redirect
 
 from educationapp.app.database import db
 from educationapp.app.models.direction import Direction
-
+from educationapp.app.models.specialty import Specialty
 
 direction_blueprint = Blueprint('direction', __name__)
 
 @direction_blueprint.route('/directions')
 def get_directions():
     directions = Direction.query.all()
-    return render_template('directions.html', directions=directions)
+    specialties = Specialty.query.all()
+    return render_template('directions.html', directions=directions, specialties=specialties)
+
 
 
 @direction_blueprint.route('/directions', methods=['POST'])
 def create_direction():
-    data = request.get_json()
-    new_direction = Direction(title=data['title'], specialty_id=data['specialty_id'])
+    title = request.form.get('title')
+    specialty_id = request.form.get('specialty_id')
+
+    if not title or not specialty_id:
+        return jsonify({'error': 'Некорректные данные'}), 400
+
+    if Direction.query.filter_by(title=title).first():
+        return jsonify({'error': 'Направление уже существует'}), 409
+
+    new_direction = Direction(title=title, specialty_id=specialty_id)
     db.session.add(new_direction)
     db.session.commit()
-    return jsonify(new_direction.to_dict()), 201
+    return redirect(url_for('direction.get_directions'))
 
 
 @direction_blueprint.route('/directions/<int:direction_id>', methods=['PUT'])
--- educationapp/app/api/specialty_routes.py
+++ educationapp/app/api/specialty_routes.py
@@ -1,4 +1,6 @@
-from flask import jsonify, request, Blueprint, render_template
+from flask import jsonify, request, Blueprint, render_template, url_for
+from werkzeug.utils import redirect
+
 from educationapp.app.database import db
 
 from educationapp.app.models.specialty import Specialty
@@ -24,15 +26,18 @@ def get_specialty(specialty_id):
 # Создание новой специальности
 @specialty_blueprint.route('/specialties', methods=['POST'])
 def create_specialty():
-    data = request.json
-    name = data.get('name')
-    cut_off_score = data.get('cut_off_score')
+    name = request.form.get('name')
+    cut_off_score = request.form.get('cut_off_score')
 
-    if not name or cut_off_score is None:
-        return jsonify({'message': 'Missing name or cut_off_score'}), 400
+    if not name or not cut_off_score:
+        return jsonify({'error': 'Некорректные данные'}), 400
+
+    if Specialty.query.filter_by(name=name).first():
+        return jsonify({'error': 'Специальность уже существует'}), 409
 
     new_specialty = Specialty(name=name, cut_off_score=cut_off_score)
     db.session.add(new_specialty)
     db.session.commit()
+    return redirect(url_for('specialty.get_specialties'))
 
-    return jsonify({'message': 'Specialty created'}), 201
+
